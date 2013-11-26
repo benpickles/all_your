f816401ceec0f4eb737396d8e1e3cfca94b48ada
Ben Pickles <spideryoung@gmail.com>
2013-11-26 18:22:33 +0000

    A few more tests.

--- test/test_base_77.rb
+++ test/test_base_77.rb
@@ -11,4 +11,17 @@ describe AllYour::Base77 do
     assert_equal %('QJRv), AllYour::Base77::encode(123456789)
     assert_equal 123456789, AllYour::Base77::decode(%('QJRv))
   end
+
+  it 'works again' do
+    assert_equal '-~9wMnVh$', AllYour::Base77::encode(12345678987654321)
+    assert_equal 12345678987654321, AllYour::Base77::decode('-~9wMnVh$')
+  end
+
+  it 'encodes a string of digits' do
+    assert_equal %('QJRv), AllYour::Base77::encode('123456789')
+  end
+
+  it 'returns an empty string for a non-positive number' do
+    assert_equal '', AllYour::Base77.encode(-999)
+  end
 end
