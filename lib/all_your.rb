0b4cba88bc6a8d80cd87d1f62eed783c294d680d
Ben Pickles <spideryoung@gmail.com>
2013-11-27 08:44:48 +0000

    Allow easy access to the "official" base 78.

--- lib/all_your.rb
+++ lib/all_your.rb
@@ -1,6 +1,19 @@
 require 'all_your/version'
 
 class AllYour
+  # I *think* this represents the characters that can safely be in a path
+  # without having to be encoded.
+  #
+  # <http://www.ietf.org/rfc/rfc3986.txt>
+  Base78 = %w(
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    a b c d e f g h i j k l m n o p q r s t u v w x y z
+    0 1 2 3 4 5 6 7 8 9
+    ! $ & ' ( ) * + , ; =
+    - _ . ~
+    /
+  ).sort!.freeze
+
   def self.base(base)
     @bases.fetch(base)
   end
@@ -45,15 +58,4 @@ class AllYour
     end
 end
 
-# Base 78: I *think* this represents the characters that can safely be in a path
-# without having to be encoded.
-#
-# <http://www.ietf.org/rfc/rfc3986.txt>
-AllYour.register(%w(
-  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-  a b c d e f g h i j k l m n o p q r s t u v w x y z
-  0 1 2 3 4 5 6 7 8 9
-  ! $ & ' ( ) * + , ; =
-  - _ . ~
-  /
-).sort!)
+AllYour.register(AllYour::Base78)
